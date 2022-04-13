17f9b5a5ff677da7e207b983281502070226df92
Rinoto Harto <rinoto.harto@phincon.com>
2022-04-13 22:09:27 +0700

    compro v1 done

--- src/containers/Portfolio/index.jsx
+++ src/containers/Portfolio/index.jsx
@@ -8,12 +8,18 @@ import Whatsapp from "../../components/WhatsApp";
 import mainimage from '../../static/images/main_image.png';
 import mainimage_kaffi from '../../static/images/mainimage_kaffi.png';
 import mainimage_kahve from '../../static/images/mainimage_kahve.png';
+import mainimage_krisdiansyah from '../../static/images/mainimage_krisdiansyah.png';
+import mainimage_adhy from '../../static/images/mainimage_adhy.png';
 import secondimage from '../../static/images/second_image.png';
 import secondimage_kaffi from '../../static/images/secondimage_kaffi.png';
 import secondimage_kahve from '../../static/images/secondimage_kahve.png';
+import secondimage_krisdiansyah from '../../static/images/secondimage_krisdiansyah.png';
+import secondimage_adhy from '../../static/images/secondimage_adhy.png';
 import thumbnail from '../../static/images/thumbnail.png';
 import thumbnail_kahve from '../../static/images/thumbnail_kahve.png';
 import thumbnail_kaffi from '../../static/images/thumbnail_kaffi.png';
+import thumbnail_krisdiansyah from '../../static/images/thumbnail_krisdiansyah.png';
+import thumbnail_adhy from '../../static/images/thumbnail_adhy.png';
 
 const Portfolio = () => {
   const history = useHistory();
@@ -23,8 +29,12 @@ const Portfolio = () => {
       mainimage: mainimage,
       secondimage: secondimage,
       title: "Girigahana UPN Jakarta",
-      description: "Dies natalis girigahana UPN veteran Jakarta yg ke 40th, memberikan kepercayaan dan memilih Techartsy dalam pembuatan undangan digital  berbasis web, dengan tujuan agar dapat menjangkau seluruh anggota yg berada di seluruh Indonesia. Dan ini momentum untuk lebih bersahabat dengan teknologi digital.",
-      result: 'Perkembangan teknologi yang pesat membuat kita dengan mudah memberikan dan mendapatakan kabar gembira untuk sesama. Undangan digital berbasis web lebih atraktif, interaktif, menghemat waktu, biaya, dan ramah lingkungan. Kami dapat membantu anda dalam hal ini',
+      description: `Dengan bangga dan senang hati, techartsy dipercaya untuk berkolaborasi dalam acara yang sakral dan penuh kebahagiaan ini.
+      Di era modern yang serba cepat, sudah seharusnya kita bersahabat dengan dunia digital.
+      Pada kolaborasi kali ini, techartsy di percaya untuk membuat digital invitation yang menarik serta user friendly agar berkesan dihati para tamu undangan.
+      `,
+      result: `Di era modern yang serba cepat, kita harus beradaptasi agar tidak jauh tertinggal. Contohnya seperti digital invitation ini, karena lebih atraktif, interaktif, hemat waktu dan biaya, serta less paper (ramah lingkungan)
+      Techartsy akan selalu siap dan senang hati berkolaborasi dengan anda.`,
       category: "Digital-Invitation",
       thumbnail: thumbnail
     },
@@ -47,6 +57,34 @@ const Portfolio = () => {
       result: 'Dengan compro yg baik dan menarik, diharapkan banyak customer tertarik dengan produk yg ditawarkan. Persiapkan compro anda dengan baik bersama Techartsy Indonesia',
       category: "Company-Profile",
       thumbnail: thumbnail_kaffi
+    },
+    {
+      id: 4,
+      mainimage: mainimage_krisdiansyah,
+      secondimage: secondimage_krisdiansyah,
+      title: "Invitation Krisdiansyah & Azmi",
+      description: `Dengan bangga dan senang hati, techartsy dipercaya untuk berkolaborasi dalam acara yang sakral dan penuh kebahagiaan ini.
+        Di era modern yang serba cepat, sudah seharusnya kita bersahabat dengan dunia digital.
+        Pada kolaborasi kali ini, techartsy di percaya untuk membuat digital invitation yang menarik serta user friendly agar berkesan dihati para tamu undangan.
+        `,
+      result: `Di era modern yang serba cepat, kita harus beradaptasi agar tidak jauh tertinggal. Contohnya seperti digital invitation ini, karena lebih atraktif, interaktif, hemat waktu dan biaya, serta less paper (ramah lingkungan)
+        Techartsy akan selalu siap dan senang hati berkolaborasi dengan anda.`,
+      category: "Digital-Invitation",
+      thumbnail: thumbnail_krisdiansyah
+    },
+    {
+      id: 5,
+      mainimage: mainimage_adhy,
+      secondimage: secondimage_adhy,
+      title: "Invitation Adhy & Yenny",
+      description: `Dengan bangga dan senang hati, techartsy dipercaya untuk berkolaborasi dalam acara yang sakral dan penuh kebahagiaan ini.
+      Di era modern yang serba cepat, sudah seharusnya kita bersahabat dengan dunia digital.
+      Pada kolaborasi kali ini, techartsy di percaya untuk membuat digital invitation yang menarik serta user friendly agar berkesan dihati para tamu undangan.
+      `,
+      result: `Di era modern yang serba cepat, kita harus beradaptasi agar tidak jauh tertinggal. Contohnya seperti digital invitation ini, karena lebih atraktif, interaktif, hemat waktu dan biaya, serta less paper (ramah lingkungan)
+      Techartsy akan selalu siap dan senang hati berkolaborasi dengan anda.`,
+      category: "Digital-Invitation",
+      thumbnail: thumbnail_adhy
     },
   ]
   const goToDetail = (item) => {
